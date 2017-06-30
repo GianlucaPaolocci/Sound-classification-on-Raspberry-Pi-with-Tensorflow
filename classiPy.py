2df6dd3daa9b2879bf285f83a5767bc8355d4ed5
Gianluca Paolocci <g.paolocci92@gmail.com>
2017-06-30 11:02:19 +0200

    Source code added

---
+++ classiPy.py
@@ -0,0 +1,88 @@
+
+import glob
+import os
+import librosa
+import numpy as np
+import tensorflow as tf
+import sounddevice
+import time
+
+duration = 3  # seconds
+sample_rate=44100
+
+
+'''0 = air_conditioner
+1 = car_horn
+2 = children_playing
+3 = dog_bark
+4 = drilling
+5 = engine_idling
+6 = gun_shot
+7 = jackhammer
+8 = siren
+9 = street_music
+10 = truck'''
+
+
+
+def extract_feature():
+    X = sounddevice.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
+    sounddevice.wait()
+    X= np.squeeze(X)
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+    ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+    features = np.vstack([features,ext_features])
+    return features
+
+
+
+
+model_path = "model/model"
+
+
+
+n_dim = 193
+n_classes = 10
+n_hidden_units_one = 256
+n_hidden_units_two = 256
+sd = 1 / np.sqrt(n_dim)
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32,[None,n_dim])
+Y = tf.placeholder(tf.float32,[None,n_classes])
+
+W_1 = tf.Variable(tf.random_normal([n_dim,n_hidden_units_one], mean = 0, stddev=sd))
+b_1 = tf.Variable(tf.random_normal([n_hidden_units_one], mean = 0, stddev=sd))
+h_1 = tf.nn.tanh(tf.matmul(X,W_1) + b_1)
+
+W_2 = tf.Variable(tf.random_normal([n_hidden_units_one,n_hidden_units_two], mean = 0, stddev=sd))
+b_2 = tf.Variable(tf.random_normal([n_hidden_units_two], mean = 0, stddev=sd))
+h_2 = tf.nn.sigmoid(tf.matmul(h_1,W_2) + b_2)
+
+W = tf.Variable(tf.random_normal([n_hidden_units_two,n_classes], mean = 0, stddev=sd))
+b = tf.Variable(tf.random_normal([n_classes], mean = 0, stddev=sd))
+y_ = tf.nn.softmax(tf.matmul(h_2,W) + b)
+
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+y_true, y_pred = None, None
+with tf.Session() as sess:
+    saver.restore(sess, model_path)
+    print "Model loaded"
+    
+    sess.run(tf.global_variables())
+    while 1:
+        feat = extract_feature()
+        y_pred = sess.run(tf.argmax(y_, 1), feed_dict={X: feat})
+        
+        print y_pred
+
+
+
---
+++ trainModel.py
@@ -0,0 +1,138 @@
+
+import glob
+import os
+import librosa
+import numpy as np
+import time
+
+'''0 = air_conditioner
+1 = car_horn
+2 = children_playing
+3 = dog_bark
+4 = drilling
+5 = engine_idling
+6 = gun_shot
+7 = jackhammer
+8 = siren
+9 = street_music'''
+
+
+def extract_feature(file_name):
+    X, sample_rate = librosa.load(file_name)
+    #print file_name
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
+    return mfccs,chroma,mel,contrast,tonnetz
+
+
+
+def parse_audio_files(parent_dir,sub_dirs,file_ext='*.wav'):
+    features, labels, name = np.empty((0,193)), np.empty(0), np.empty(0)
+    for label, sub_dir in enumerate(sub_dirs):
+        print sub_dir
+        for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
+            mfccs, chroma, mel, contrast, tonnetz = extract_feature(fn)
+            ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            features = np.vstack([features,ext_features])
+            labels = np.append(labels, fn.split('-')[1])
+    return np.array(features), np.array(labels, dtype = np.int)
+
+def one_hot_encode(labels):
+    n_labels = len(labels)
+    n_unique_labels = len(np.unique(labels))
+    one_hot_encode = np.zeros((n_labels,n_unique_labels))
+    one_hot_encode[np.arange(n_labels), labels] = 1
+    return one_hot_encode
+
+
+parent_dir = 'UrbanSound8K/audio'
+
+sub_dirs = ['fold1', 'fold2', 'fold3', 'fold4', 'fold5', 'fold6', 'fold7', 'fold8', 'fold9', 'fold10']
+
+
+features, labels = parse_audio_files(parent_dir,sub_dirs)
+
+labels = one_hot_encode(labels)
+
+train_test_split = np.random.rand(len(features)) < 0.70
+train_x = features[train_test_split]
+train_y = labels[train_test_split]
+test_x = features[~train_test_split]
+test_y = labels[~train_test_split]
+
+
+# --------------------------------------------------------------------------------------------------------------------------
+
+# #### Training Neural Network with TensorFlow
+
+
+import tensorflow as tf
+from sklearn.metrics import precision_recall_fscore_support
+
+training_epochs = 5000
+n_dim = features.shape[1]
+n_classes = 10
+n_hidden_units_one = 256
+n_hidden_units_two = 256
+sd = 1 / np.sqrt(n_dim)
+learning_rate = 0.1
+model_path = "model/model"
+
+X = tf.placeholder(tf.float32,[None,n_dim])
+Y = tf.placeholder(tf.float32,[None,n_classes])
+
+W_1 = tf.Variable(tf.random_normal([n_dim,n_hidden_units_one], mean = 0, stddev=sd))
+b_1 = tf.Variable(tf.random_normal([n_hidden_units_one], mean = 0, stddev=sd))
+h_1 = tf.nn.tanh(tf.matmul(X,W_1) + b_1)
+
+
+W_2 = tf.Variable(tf.random_normal([n_hidden_units_one,n_hidden_units_two], mean = 0, stddev=sd))
+b_2 = tf.Variable(tf.random_normal([n_hidden_units_two], mean = 0, stddev=sd))
+h_2 = tf.nn.sigmoid(tf.matmul(h_1,W_2) + b_2 )
+
+
+W = tf.Variable(tf.random_normal([n_hidden_units_two,n_classes], mean = 0, stddev=sd))
+b = tf.Variable(tf.random_normal([n_classes], mean = 0, stddev=sd))
+y_ = tf.nn.softmax(tf.matmul(h_2,W) + b)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+cost_function = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(y_), reduction_indices=[1]))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
+
+correct_prediction = tf.equal(tf.argmax(y_,1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+y_true, y_pred = None, None
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(training_epochs):
+        _,cost = sess.run([optimizer,cost_function],feed_dict={X:train_x,Y:train_y})
+        cost_history = np.append(cost_history,cost)
+        print "Epoch: ", epoch, " cost ", cost
+
+    y_pred = sess.run(tf.argmax(y_,1),feed_dict={X: test_x})
+    y_true = sess.run(tf.argmax(test_y,1))
+
+    #saving model
+    save_path = saver.save(sess, model_path)
+    print("Model saved in file: %s" % save_path)
+
+
+p,r,f,s = precision_recall_fscore_support(y_true, y_pred)#, average='micro')
+print ("F-Score:"), f
+print ("Precision:"), p
+print ("Recall:"), r
+print ("Support:"), s
+
+
+
+
+
+
